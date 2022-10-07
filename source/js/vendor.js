161c5f9ee5f936b13df2c04c93e3675882dcbc5a
Etel <Etel@Stefano.local>
2022-10-07 20:16:29 +0300

    Edits on comments-3

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -34,7 +34,7 @@ const sliderReviews = new Swiper('.reviews__slider', {
   direction: 'horizontal',
   slidesPerView: 1,
   spaceBetween: 0,
-  loop: true,
+  loop: false,
   // Navigation arrows
   navigation: {
     nextEl: '.reviews__slider-button--next',
